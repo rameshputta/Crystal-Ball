a7bf06467bbef309a0606538e52a51544ad08a9c
root <root@rsharma-centos7.jiwiredev.com>
2017-08-23 11:20:43 -0400

    terminal rama edited this

--- php/terminal.php
+++ php/terminal.php
@@ -21,7 +21,7 @@ function getData1(){
 <input id="searchStr" style="height: 50px; width: 350px;" name="command" type="text" placeholder="Search.." onkeypress="return mykeypress(event)" />
 
 <?php
-echo "yp"
+echo "n,asd,amda,dma."
 $command= 'ls';
 if(isset($_GET['command']))
 	$command = $_GET['command'];
